ad45eddae8c2e93ec0d57a6b92600c7630011ca9
deepesh9001 <dipubkn14@gmail.com>
2021-11-13 21:34:43 +0530

    getSize api added

--- app.js
+++ app.js
@@ -49,11 +49,40 @@ app.post('/api/maleria', uploadImage.single('maleria'), function(req, res) {
   }
 });
 
+app.post('/api/size', uploadImage.single('size'), function(req, res) {
+  if(req.file === undefined){  
+    console.log("Image not found!")
+    return res.send({
+        success: false,
+        code: 400,
+        message: null
+    })
+  }
+
+  console.log("excuting script")
+
+  try{
+      console.log('python3 ' + __dirname + '/Malarial/backendScript2.py ' + req.file.filename)
+      var child = exec('python3 ' + __dirname + '/Malarial/backendScript2.py ' + req.file.filename)
+      child.stdout.on('data', (data) => res.send({
+        success: true,
+        code: 200,
+        message: "Operation successful",
+        response: JSON.parse(data)
+    }))
+  } catch (err) {
+      console.log(err)
+      return res.send({
+          success: false,
+          code: 502,
+          message:'Internal server error'
+      })
+  }
+});
 
 
 app.get('/images/:img', function (req, res) {
   const pathToImg = __dirname + '/Malarial/images/'+ req.params.img
-  console.log(pathToImg)
   try{
       if(!fs.existsSync(pathToImg)) return res.send({
           success: false,
